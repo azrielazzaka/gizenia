dd84a10577b878e2c145fd720dff7abf5b39cf13
angga236 <yuskidesa@gmail.com>
2026-05-25 12:58:39 +0700

    update api endpoints

--- mobile-flutter/lib/core/constants/api_endpoints.dart
+++ mobile-flutter/lib/core/constants/api_endpoints.dart
@@ -7,7 +7,7 @@ class ApiEndpoints {
   static const String register = "$baseUrlLaravel/auth/register";
   static const String sendOtp = "$baseUrlLaravel/auth/send-otp"; 
   static const String getProfile = "$baseUrlLaravel/user/profile";
-  static const String updateProfile = "$baseUrlLaravel/user/profile/update";
+  static const String updateProfile = "$baseUrlLaravel/user/profile/update ";
   static const String getHistory = "$baseUrlLaravel/user/history";
   static const String saveHistory = "$baseUrlLaravel/user/history/add";
   static const String getMenus = "$baseUrlLaravel/user/menus";
